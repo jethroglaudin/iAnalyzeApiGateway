d72c734f024808a984516f36e4c92d5b00b4df51
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-05-10 23:09:59 -0400

    Gateway connected thus far to Auth microservice

---
+++ src/test/java/com/iAnalyze/apiGateway/iAnalyzeApiGateway/IAnalyzeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iAnalyze.apiGateway.iAnalyzeApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAnalyzeApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
